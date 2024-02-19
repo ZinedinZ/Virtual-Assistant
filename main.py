a62fbbd10714229f2598dfefa8b5d6f95c03d751
ZinedinZ <zinedi_zulic@live.com>
2024-02-20 00:57:09 +0100

    Added bot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import json
+from difflib import get_close_matches
 
 window = tk.Tk()
 window.title("Virtual Assistant")
@@ -17,13 +19,56 @@ fromEntry.grid(row=1, column=0, padx=10, sticky="ew")
 
 textInput = tk.Entry(master=fromEntry)
 textInput.pack(fill=tk.BOTH, expand=True)
+textInput.bind("<Return>", "svaka cast")
+textInput.insert(0, "Writte your message here")
 
-sendButton = tk.Button(master=window, text="send")
+def send_message(event=None):
+    knowledge = load_knowledge_base('knowledge_base.json')
+    while True:
+        text = textInput.get()
+        if text.lower() == 'quit':
+            break
+        else:
+            messages.insert(tk.END, f"You: {text}")
+        best_match = best_match(text, [q["question"] for q in knowledge])
+
+        if best_match:
+            answer = get_answer_for_question(best_match, knowledge)
+            messages.insert(tk.END, f"Bot: {answer}")
+
+        else:
+            messages.insert(tk.END, f"Bot: I dont know")
+            new_answer = textInput.get()
+            if new_answer.lower() != 'skip':
+                knowledge["question"].append({"question": text, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json',knowledge)
+
+sendButton = (tk.Button(master=window, text="send", command=send_message))
+textInput.bind("<Return>", send_message)
 sendButton.grid(row=1, column=1, pady=10, sticky="ew")
 
 window.rowconfigure(0, minsize=500, weight=1)
 window.rowconfigure(1, minsize=50, weight=0)
 window.columnconfigure(0, minsize=500, weight=1)
 window.columnconfigure(1, minsize=250, weight=0)
+
+def load_knowledge_base(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def save_knowledge_base(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
+
+def best_match(user_question, questions):
+    matches = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+def get_answer_for_question(question, knowledge_base):
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
+
 
 window.mainloop()
